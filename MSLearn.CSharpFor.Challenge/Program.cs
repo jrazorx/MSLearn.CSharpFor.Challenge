fc66aa0be74bf6f1eb82d94bf4cf86ace5f26292
jra <julien.gault.50@gmail.com>
2024-02-27 15:23:46 +0100

    solution FizzBuzz

--- MSLearn.CSharpFor.Challenge/Program.cs
+++ MSLearn.CSharpFor.Challenge/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿for (int i = 1; i <= 100; i++)
+{
+    bool isFizz = i % 3 == 0 ? true : false;
+    bool isBuzz = i % 5 == 0 ? true : false;
+
+    Console.Write(i);
+
+    if (isFizz && isBuzz)
+        Console.WriteLine(" - FizzBuzz");
+    else if (isFizz)
+        Console.WriteLine(" - Fizz");
+    else if (isBuzz)
+        Console.WriteLine(" - Buzz");
+    else
+        Console.WriteLine();
+}
